df9a9dc65678a84b08b02ca614234a5c73c35bb6
Brandon_Hart <sugb73@gmail.com>
2022-08-21 23:11:34 -0400

    Made changes to the Navbar and added HomePage.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Routes>
+        <Route path='/' exact component={HomePage} />
+      </Routes>
     </Router>
   );
 }
